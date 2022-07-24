eb55d58b2d28ae31cee381c17efd031405889201
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-24 11:24:05 -0400

    react project-1 4-7

--- 1-react/src/index.js
+++ 1-react/src/index.js
@@ -89,12 +89,13 @@ class Game extends React.Component {
             history: [{
                 cuadrados: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const cuadrados = current.cuadrados.slice();
 
@@ -106,15 +107,34 @@ class Game extends React.Component {
             history: history.concat([{
                 cuadrados: cuadrados,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
     render() {
 
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calcularGanador(current.cuadrados);
+
+    const moves = history.map((step, move) => {
+        const desc = move ?
+        'Go to move #' + move:
+        'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
 
     let status;
     if(winner) {
@@ -133,7 +153,7 @@ class Game extends React.Component {
             </div>
             <div className="game-info">
                 <div>{status}</div>
-                <ol>{/* TODO */}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
