968f1e2a8ad837d8ddec10974e182fdab4d4816d
Marco Rossel <andresmarcorossel@gmail.com>
2025-05-03 00:01:26 -0400

    typescript | registrar nuevo usuario con zod y react hook form

--- src/app/registrarusuario/page.tsx
+++ src/app/registrarusuario/page.tsx
@@ -1,20 +1,31 @@
 "use client"
 import style from "./registrarusuario.module.css";
 import { SubmitHandler, useForm } from "react-hook-form"
+import { registerSchema as schema, plans } from "@/validations/registrarUsuario";
+import { zodResolver } from '@hookform/resolvers/zod';
+//import { zodResolver } from '@hookform/resolvers/zod';
 
 type Input = {
-  nombre: string,
-  password: string,
-  passwordReview: string,
-  email: string,
+  nombre: string
+  password: string
+  confirmPassword: string
+  email: string
+  peso: string
+  plan: string
 }
 
 export default function RegistrarUsuario () {
 
-  const {register, handleSubmit, formState:{errors}} = useForm<Input>()
+  const {register, handleSubmit, formState:{errors}} = useForm<Input>({resolver: zodResolver(schema)})
 
-  const onSubmit: SubmitHandler<Input> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<Input> = (data) => {
+    console.log(data);
+    console.log(errors);
+  }
 
+  const opcionesPlan = plans.map(val => {
+    return (<option key={val} value={val}>{val}</option>)
+  })
   
   return (
     <div className={style.main}>
@@ -25,29 +36,42 @@ export default function RegistrarUsuario () {
         <input
         {...register("nombre", { required: true })}
         type="text" name="nombre" id="nombre" placeholder="nombre..." />
-        {errors.nombre && <span>Please include a name please</span>}
+        {errors.nombre && <span>{errors.nombre.message}</span>}
 
         {/* email */}
         <label htmlFor="email">email</label>
         <input 
         {...register("email", {required: true})}
         type="email" name="email" id="email" placeholder="example@gmail.com" />
-        {errors.email && <span>Please include an email please</span>}
+        {errors.email && <span>{errors.email.message}</span>}
 
         {/* password */}
         <label htmlFor="password">password</label>
         <input 
         {...register("password", {required: true})}
         type="password" name="password" id="password" placeholder="*********" />
-        {errors.password && <span>Please include a password please</span>}
+        {errors.password && <span>{errors.password.message}</span>}
 
         {/* password */}
-        <label htmlFor="password">password</label>
+        <label htmlFor="confirmPassword">confirm password</label>
         <input 
-        {...register("passwordReview", {required: true})}
-        type="password" name="passwordReview" id="passwordReview" placeholder="*********" />
-        {errors.passwordReview && <span>Please repeat the same password please</span>}
+        {...register("confirmPassword", {required: true})}
+        type="password" name="confirmPassword" id="confirmPassword" placeholder="*********" />
+        {errors.confirmPassword && <span>{errors.confirmPassword.message}</span>}
 
+
+        <label htmlFor="peso">peso</label>
+        <input
+        {...register("peso")}
+         type="number" name="peso" id="peso" placeholder="0" />
+        {errors.peso && <span>{errors.peso.message}</span>}
+
+        {/* plan */}
+        <label htmlFor="plan">Plan</label>
+        <select id="plan" {...register("plan")}>
+          {opcionesPlan}
+        </select>
+        {errors.plan?.message && <p>{errors.plan?.message}</p>}
 
         {/* por ahora resivimos todo bien pero nesecitamos una comparacion de password
         si escribimos password diferentes lo acepta sin problemas.
---
+++ src/validations/registrarUsuario.ts
@@ -0,0 +1,41 @@
+import {z} from "zod"
+
+// as const por que z.enum resive valores de solo lectura, si nos marca error.
+export const plans = ["free", "basic", "medium", "pro"] as const
+
+export type Plan = (typeof plans)[number] // para iterar en el componente front y solo editar un array en futuros cambios
+
+export const registerSchema = z.object({
+
+
+  nombre: z.string()
+  .min(4, {message: "Name must be at least 4 characters long"})
+  .max(50, {message: "Name must be less than 50 characters long"}),
+  
+
+  password: z.string().min(6, {message: "Password must be at least 6 characters long"}),
+
+
+  confirmPassword: z.string(),
+
+
+  email: z.string().email({message: "invalid email"}),
+  // codifo de ejemplo si un input type number es resivido:
+  // peso: z.string() <=== es string por que al momento de resivir es si o si string
+  
+  // luego .refine(prev => mutarValorPrevio(prev)) lo mutamos a nuestra conveniencia.
+  //peso: z.string().refine(peso => parseFloat(peso))
+
+  // ejemplo si el imput no es un numero y lo queremos transformar a number con parseFoat:
+  // con el envio de un mensaje de error
+  peso: z.string().refine(previo => !isNaN(parseFloat(previo)), {message:"send a number please"}),
+
+
+  plan: z.enum(plans, {errorMap: ()=> ({message: "Please select a plan"})})
+
+  
+})
+.refine(data => data.password === data.confirmPassword, {
+  message: "passwords must match", 
+  path: ["confirmPassword"] // path es la ruta donde se mostrara el mensaje de error
+})
