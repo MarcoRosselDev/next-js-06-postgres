abe3b2c2c3ea81440d5b8092570ceddd006516d4
Marco Rossel <andresmarcorossel@gmail.com>
2025-05-06 00:31:52 -0400

    typescript | error handler with react-hook-form and zod

--- src/app/verbs/newverb/page.tsx
+++ src/app/verbs/newverb/page.tsx
@@ -1,31 +1,50 @@
+"use client"
 import Link from "next/link";
-import style from "./newverb.module.css";
+import {useForm, SubmitHandler} from "react-hook-form"
+import { Input } from "./verb_type"
+import { zodResolver } from '@hookform/resolvers/zod';
+import {newVerbSchema as schema} from "./handleErros"
 
 export default function NewVerb(){
+  
+  const {register, handleSubmit, formState:{errors}} = useForm({resolver: zodResolver(schema)})
+
+  const handleForm: SubmitHandler<Input> = (data) => console.log(data);
+  
   return (
     <main>
-      <form className={style.form_newverb}>
+      <form onSubmit={handleSubmit(handleForm)}>
         <h3>New verb</h3>
 
         <label htmlFor="regularverb">regular verb</label>
-        <input type="text" name="regularverb" id="regularverb" placeholder="...go" />
-        <span>please enter a new message</span>
+        <input
+        {...register("regularverb", {required: true, maxLength: 20})}
+        type="text" name="regularverb" id="regularverb" placeholder="...go" />
+        <span>{errors.regularverb && errors.regularverb.message}</span>
 
         <label htmlFor="simplepast">simple past</label>
-        <input type="text" name="simplepast" id="simplepast" placeholder="...went" />
-        <span>please enter a new message</span>
+        <input
+        {...register("simplepast", {required: true, maxLength: 20})}
+        type="text" name="simplepast" id="simplepast" placeholder="...went" />
+        <span>{errors.simplepast && errors.simplepast.message}</span>
 
         <label htmlFor="presentperfect">present perfect</label>
-        <input type="text" name="presentperfect" id="presentperfect" placeholder="...have gone/has gone" />
-        <span>please enter a new message</span>
+        <input
+        {...register("presentperfect", {required: true, maxLength: 20})}
+        type="text" name="presentperfect" id="presentperfect" placeholder="...have gone/has gone" />
+        <span>{errors.presentperfect && errors.presentperfect.message}</span>
 
         <label htmlFor="presentcontinuous">present continuous</label>
-        <input type="text" name="presentcontinuous" id="presentcontinuous" placeholder="...I'm going" />
-        <span>please enter a new message</span>
+        <input
+        {...register("presentcontinuous", {required: true, maxLength: 20})}
+        type="text" name="presentcontinuous" id="presentcontinuous" placeholder="...I'm going" />
+        <span>{errors.presentcontinuous && errors.presentcontinuous.message}</span>
 
         <label htmlFor="infinitivo">infinitivo</label>
-        <input type="text" name="infinitivo" id="infinitivo" placeholder="...to go" />
-        <span>please enter a new message</span>
+        <input
+        {...register("infinitivo", {required: true, maxLength: 20})}
+        type="text" name="infinitivo" id="infinitivo" placeholder="...to go" />
+        <span>{errors.infinitivo && errors.infinitivo.message}</span>
       
         <button type="submit">send</button>
 
---
+++ src/app/verbs/newverb/verb_type.ts
@@ -0,0 +1,7 @@
+export type Input = {
+  regularverb: string
+  simplepast: string
+  presentperfect: string
+  presentcontinuous: string
+  infinitvo: string
+}
