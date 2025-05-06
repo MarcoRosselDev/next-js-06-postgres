c8b0379416ae194edec5294467ab6ec78cf76a94
Marco Rossel <andresmarcorossel@gmail.com>
2025-05-05 23:43:20 -0400

    typescript | verbs project, new verb page

--- src/app/verbs/newverb/page.tsx
+++ src/app/verbs/newverb/page.tsx
@@ -1,25 +1,36 @@
 import Link from "next/link";
+import style from "./newverb.module.css";
 
 export default function NewVerb(){
   return (
-    <div>
-      <form>
+    <main>
+      <form className={style.form_newverb}>
+        <h3>New verb</h3>
+
         <label htmlFor="regularverb">regular verb</label>
         <input type="text" name="regularverb" id="regularverb" placeholder="...go" />
+        <span>please enter a new message</span>
 
         <label htmlFor="simplepast">simple past</label>
         <input type="text" name="simplepast" id="simplepast" placeholder="...went" />
+        <span>please enter a new message</span>
 
         <label htmlFor="presentperfect">present perfect</label>
         <input type="text" name="presentperfect" id="presentperfect" placeholder="...have gone/has gone" />
-      
+        <span>please enter a new message</span>
+
         <label htmlFor="presentcontinuous">present continuous</label>
         <input type="text" name="presentcontinuous" id="presentcontinuous" placeholder="...I'm going" />
+        <span>please enter a new message</span>
 
         <label htmlFor="infinitivo">infinitivo</label>
         <input type="text" name="infinitivo" id="infinitivo" placeholder="...to go" />
+        <span>please enter a new message</span>
+      
+        <button type="submit">send</button>
+
       </form>
       <Link href="/verbs" >go back</Link>
-    </div>
+    </main>
   )
 }
